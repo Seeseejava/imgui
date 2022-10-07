c1716f4dbf31c2ce140df5c47647bc69cd775065
Seeseejava <49669869+Seeseejava@users.noreply.github.com>
2022-10-07 11:10:20 +0800

    Update for Flame

--- premake5.lua
+++ premake5.lua
@@ -1,150 +1,46 @@
-workspace "Flame"
-    architecture "x64"
-    startproject "Sandbox"
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	--staticruntime "off"
+	staticruntime "on"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
---Include directories relative to root folder(solution directory)
-IncludeDir = {}
-IncludeDir["GLFW"] = "Flame/vendor/GLFW/include"
-IncludeDir["Glad"] = "Flame/vendor/Glad/include"
-IncludeDir["ImGui"] = "Flame/vendor/imgui"
-IncludeDir["glm"] = "Flame/vendor/glm"
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
 
+	filter "system:windows"
+		systemversion "latest"
+		--cppdialect "C++17"
 
-include "Flame/vendor/GLFW"
-include "Flame/vendor/Glad"
-include "Flame/vendor/imgui"
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		--cppdialect "C++17"
 
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-project "Flame"
-    location "Flame"
-    kind "SharedLib" --这样可以include a static library
-    language "C++"
-    staticruntime "off"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    pchheader "flamepch.h"
-    pchsource "Flame/src/flamepch.cpp"
-    
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp",
-        "%{prj.name}/vendor/glm/glm/**.hpp",
-        "%{prj.name}/vendor/glm/glm/**.inl"
-    }
-
-    
-    includedirs
-    {
-        "%{prj.name}/src",
-        "%{prj.name}/vendor/spdlog/include",
-        "%{IncludeDir.GLFW}",
-        "%{IncludeDir.Glad}",
-        "%{IncludeDir.ImGui}",
-        "%{IncludeDir.glm}",
-    }
-
-    links
-    {
-        "GLFW", 
-        "Glad",
-        "ImGui",
-        "opengl32.lib"
-    }
-
-    filter "system:windows"
-        cppdialect "C++17"
-        systemversion "latest"
-
-        defines 
-        {
-            "FLAME_PLATFORM_WINDOWS",
-            "FLAME_BUILD_DLL",   
-            "GLFW_INCLUDE_NONE"
-        }
-
-        postbuildcommands
-        {
-            --("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-            --优化第一次build失败问题(但是语法不懂)
-            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
-
-        }
-
-    filter "configurations:Debug"
-        defines "FLAME_DEBUG"
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines "FLAME_RELEASE"
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "FLAME_DIST"
-        runtime "Release"
-        optimize "On"
-
-project "Sandbox"
-        location "Sandbox"
-        kind "ConsoleApp"
-        language "C++"
-        staticruntime "Off"--这里为什么还是设置未off
-
-        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-        files
-        {
-            "%{prj.name}/src/**.h",
-            "%{prj.name}/src/**.cpp",
-        }
-
-        includedirs
-        {
-            "Flame/vendor/spdlog/include",
-            "Flame/src",
-            "%{IncludeDir.glm}",
-        }
-
-        links
-        {
-            "Flame"
-        }
-
-    filter "system:windows"
-        cppdialect "C++17"
-        --staticruntime "On"
-        systemversion "latest"
-
-        defines 
-        {
-            "FLAME_PLATFORM_WINDOWS";         
-        }
-
-
-    filter "configurations:Debug"
-        defines "FLAME_DEBUG"
-        runtime "Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        defines "FLAME_RELEASE"
-        runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        defines "FLAME_DIST"
-        runtime "Release"
-        optimize "On"
+	-- filter "configurations:Dist"
+	-- 	runtime "Release"
+	-- 	optimize "on"
+	-- 	symbols "off"
